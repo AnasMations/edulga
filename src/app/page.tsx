c9af05afceca3e8c98142a80ee2cf9b0c96463f0
Anas Ahmed <anas.ahassan@yahoo.com>
2024-10-21 21:21:18 +0300

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,69 @@
+import IconEdulga from "@/assets/IconEdulga";
+import Header from "@/components/Header";
+import KnowledgeGraph from "@/components/KnowledgeGraph";
 import Image from "next/image";
+import graphTemp from "@/assets/graph.png";
+import AiAvatar from "@/components/AiAvatar";
+
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="">
+      <Header />
+      <div className="h-[108px]" />
+      <div className="p-8">
+        <div className="flex w-full justify-center items-start gap-8">
+          {/* Left Column */}
+          <div className="flex flex-col gap-8">
+            {/* <KnowledgeGraph/> */}
+            <div className="bg-white w-[800px] h-[500px] rounded-2xl shadow-lg p-4">
+              <div className="bg-gray-300 w-full h-full rounded-2xl overflow-hidden">
+                <Image
+                  src={graphTemp}
+                  alt=""
+                  className="w-full h-full object-cover"
+                ></Image>
+              </div>
+            </div>
+            {/* Roadmap */}
+            <div className="bg-white w-[800px] h-[500px] rounded-2xl shadow-lg">
+              <div className="bg-gray-300 w-full h-[64px] rounded-t-2xl flex items-center justify-start p-4">
+                <b>Learning</b>
+              </div>
+            </div>
+          </div>
+          {/* Right Column */}
+          <div className="flex flex-col gap-8">
+            {/* <AiAvatar/> */}
+            <div className="bg-white w-[500px] h-[500px] rounded-2xl shadow-lg p-4 overflow-hidden">
+              <AiAvatar />
+            </div>
+            {/* Communities */}
+            <div className="bg-white w-[500px] rounded-2xl shadow-lg">
+              <div className="bg-gray-300 w-full h-[64px] rounded-t-2xl flex items-center justify-start p-4">
+                <b>Community</b>
+              </div>
+              <div className="p-4 flex gap-4 flex-wrap">
+                <div className="bg-[#FFCB86] py-2 px-4 w-fit rounded flex flex-col cursor-pointer hover:bg-opacity-70">
+                  <b>Intro to NLP</b>
+                  <p className=" text-xs">40 members</p>
+                </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                <div className="bg-[#FFCB86] py-2 px-4 w-fit rounded flex flex-col cursor-pointer hover:bg-opacity-70">
+                  <b>AI enthusiasts</b>
+                  <p className=" text-xs">+99 members</p>
+                </div>
+
+                <div className="bg-[#FF4086] py-2 px-4 w-fit rounded flex flex-col cursor-pointer hover:bg-opacity-70 text-white">
+                  <b>Getting into the world of NLP</b>
+                  <p className=" text-xs">Event - 20th Oct 2025 - 19:30 to 21:30</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
---
+++ src/components/KnowledgeGraph.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+
+interface Props {
+    clasName?: string;
+}
+
+const KnowledgeGraph = ({ clasName }: Props) => {
+
+    return <div></div>;
+}
+
+export default KnowledgeGraph;
